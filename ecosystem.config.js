5a2dfd0e15448d68959fe836951126d60cdccfb4
SahanDhanujaya <sahandhanujaya18@gmail.com>
2026-03-30 20:05:30 +0530

    update pm2 config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,19 @@
 module.exports = {
   apps : [
+      {
+        name: "config-server",
+        script: "java -jar ./config-server/target/Config-Server-0.0.1-SNAPSHOT.jar",
+        log_file: "./log/config-server.log"
+      },
     {
-      name: 'config-server',
-      script: './config-server/target/config-server-0.0.1-SNAPSHOT.jar',
-      interpreter: 'java',
-      args: '-Xmx256m', // Optional: Limits RAM to save money on GCP
-      out_file: "./log/config-server-out.log",
-      error_file: "./log/config-server-error.log",
-      merge_logs: true
+      name: "service-registry",
+      script: "java -jar ./service-registry/target/Service-Registry-0.0.1-SNAPSHOT.jar",
+      log_file: "./log/service-registry.log"
     },
     {
-      name: 'service-registry',
-      script: './service-registry/target/eureka-server-0.0.1-SNAPSHOT.jar',
-      interpreter: 'java',
-      args: '-Xmx256m',
-      out_file: "./log/service-registry-out.log",
-      error_file: "./log/service-registry-error.log",
-      merge_logs: true
-    },
-    {
-      name: 'api-gateway',
-      script: './api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar',
-      interpreter: 'java',
-      args: '-Xmx256m',
-      out_file: "./log/api-gateway-out.log",
-      error_file: "./log/api-gateway-error.log",
-      merge_logs: true
+      name: "api-gateway",
+      script: "java -jar ./api-gateway/target/Api-Gateway-0.0.1-SNAPSHOT.jar",
+      log_file: "./log/api-gateway.log"
     }
   ],
 };
