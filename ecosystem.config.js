f33980a3d73570bab0efb75dc8929f039ecbdb72
SahanDhanujaya <sahandhanujaya18@gmail.com>
2026-03-30 20:21:13 +0530

    update pm2 config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
     {
       name: "config-server",
-      script: "./config-server/target/Config-Server-0.0.1-SNAPSHOT.jar",
+      script: "./config-server/target/config-server-0.0.1-SNAPSHOT.jar",
       args: "",
       interpreter: "java",
       interpreter_args: "-jar",
@@ -10,14 +10,14 @@ module.exports = {
     },
     {
       name: "service-registry",
-      script: "./service-registry/target/Service-Registry-0.0.1-SNAPSHOT.jar",
+      script: "./service-registry/target/eureka-server-0.0.1-SNAPSHOT.jar",
       interpreter: "java",
       interpreter_args: "-jar",
       log_file: "./log/service-registry.log"
     },
     {
       name: "api-gateway",
-      script: "./api-gateway/target/Api-Gateway-0.0.1-SNAPSHOT.jar",
+      script: "./api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar",
       interpreter: "java",
       interpreter_args: "-jar",
       log_file: "./log/api-gateway.log"
