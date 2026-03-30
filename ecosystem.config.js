2d1179d2147a08f713732658fff84da130608e10
SahanDhanujaya <sahandhanujaya18@gmail.com>
2026-03-30 20:18:07 +0530

    update pm2 config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,25 @@
 module.exports = {
   apps : [
-      {
-        name: "config-server",
-        script: "java -jar ./config-server/target/Config-Server-0.0.1-SNAPSHOT.jar",
-        log_file: "./log/config-server.log"
-      },
+    {
+      name: "config-server",
+      script: "./config-server/target/Config-Server-0.0.1-SNAPSHOT.jar",
+      args: "",
+      interpreter: "java",
+      interpreter_args: "-jar",
+      log_file: "./log/config-server.log"
+    },
     {
       name: "service-registry",
-      script: "java -jar ./service-registry/target/Service-Registry-0.0.1-SNAPSHOT.jar",
+      script: "./service-registry/target/Service-Registry-0.0.1-SNAPSHOT.jar",
+      interpreter: "java",
+      interpreter_args: "-jar",
       log_file: "./log/service-registry.log"
     },
     {
       name: "api-gateway",
-      script: "java -jar ./api-gateway/target/Api-Gateway-0.0.1-SNAPSHOT.jar",
+      script: "./api-gateway/target/Api-Gateway-0.0.1-SNAPSHOT.jar",
+      interpreter: "java",
+      interpreter_args: "-jar",
       log_file: "./log/api-gateway.log"
     }
   ],
